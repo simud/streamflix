b0cb9eb153baeadb322eb66355d0195bbdf0d81f
Simud <149002960+simud@users.noreply.github.com>
2025-05-28 16:45:40 +0200

    Create R.kt

---
+++ app/src/main/java/com/tanasi/streamflix/utils/R.kt
@@ -0,0 +1,65 @@
+package com.tanasi.streamflix
+object R {
+    object string {
+        const val player_aspect_ratio_fit = 1
+        const val player_aspect_ratio_fill = 2
+        const val player_aspect_ratio_zoom = 3
+        const val player_settings_caption_style_font_color_white = 4
+        const val player_settings_caption_style_font_color_yellow = 5
+        const val player_settings_caption_style_font_color_green = 6
+        const val player_settings_caption_style_font_color_cyan = 7
+        const val player_settings_caption_style_font_color_blue = 8
+        const val player_settings_caption_style_font_color_magenta = 9
+        const val player_settings_caption_style_font_color_red = 10
+        const val player_settings_caption_style_font_color_black = 11
+        const val player_settings_caption_style_text_size_0_5 = 12
+        const val player_settings_caption_style_text_size_0_75 = 13
+        const val player_settings_caption_style_text_size_1 = 14
+        const val player_settings_caption_style_text_size_1_25 = 15
+        const val player_settings_caption_style_text_size_1_5 = 16
+        const val player_settings_caption_style_text_size_2 = 17
+        const val player_settings_caption_style_text_size_3 = 18
+        const val player_settings_caption_style_font_opacity_0_5 = 19
+        const val player_settings_caption_style_font_opacity_0_75 = 20
+        const val player_settings_caption_style_font_opacity_1 = 21
+        const val player_settings_caption_style_edge_style_none = 22
+        const val player_settings_caption_style_edge_style_raised = 23
+        const val player_settings_caption_style_edge_style_depressed = 24
+        const val player_settings_caption_style_edge_style_outline = 25
+        const val player_settings_caption_style_edge_style_drop_shadow = 26
+        const val player_settings_caption_style_background_color_black = 27
+        const val player_settings_caption_style_background_color_yellow = 28
+        const val player_settings_caption_style_background_color_green = 29
+        const val player_settings_caption_style_background_color_cyan = 30
+        const val player_settings_caption_style_background_color_blue = 31
+        const val player_settings_caption_style_background_color_magenta = 32
+        const val player_settings_caption_style_background_color_red = 33
+        const val player_settings_caption_style_background_color_white = 34
+        const val player_settings_caption_style_background_opacity_0 = 35
+        const val player_settings_caption_style_background_opacity_0_25 = 36
+        const val player_settings_caption_style_background_opacity_0_5 = 37
+        const val player_settings_caption_style_background_opacity_0_75 = 38
+        const val player_settings_caption_style_background_opacity_1 = 39
+        const val player_settings_caption_style_window_color_black = 40
+        const val player_settings_caption_style_window_color_yellow = 41
+        const val player_settings_caption_style_window_color_green = 42
+        const val player_settings_caption_style_window_color_cyan = 43
+        const val player_settings_caption_style_window_color_blue = 44
+        const val player_settings_caption_style_window_color_magenta = 45
+        const val player_settings_caption_style_window_color_red = 46
+        const val player_settings_caption_style_window_color_white = 47
+        const val player_settings_caption_style_window_opacity_0 = 48
+        const val player_settings_caption_style_window_opacity_0_25 = 49
+        const val player_settings_caption_style_window_opacity_0_5 = 50
+        const val player_settings_caption_style_window_opacity_0_75 = 51
+        const val player_settings_caption_style_window_opacity_1 = 52
+        const val player_settings_speed_0_25 = 53
+        const val player_settings_speed_0_5 = 54
+        const val player_settings_speed_0_75 = 55
+        const val player_settings_speed_1 = 56
+        const val player_settings_speed_1_25 = 57
+        const val player_settings_speed_1_5 = 58
+        const val player_settings_speed_1_75 = 59
+        const val player_settings_speed_2 = 60
+    }
+}
